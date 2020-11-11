b19c23a9935f4f997e7d9dde24580798569c6c9a
Subhash <Subhash@Subhash-PC.home>
2020-11-11 23:04:46 +0100

    My first commit

---
+++ src/com/subhash/javabasics/helloworld/HelloWorld.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.subhash.javabasics.helloworld;
+
+/**
+ * @author Subhash
+ * 
+ * Hello World
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Hello World...!!!!");
+
+	}
+
+}
